2dd4c51108e0b1df0b04bd50856ffebcc5b84282
Rafael hajime mori <hajimemori97@gmail.com>
2021-10-29 20:42:02 -0300

    Atualização do projeto CRUD em JS

--- JavaScript na Web/JS na Web- CRUD com JS/2016-JS-na-web-crud-com-JavaScript-assincrono-projeto_inicial/projeto_inicial/admin/service/cliente-service.js
+++ JavaScript na Web/JS na Web- CRUD com JS/2016-JS-na-web-crud-com-JavaScript-assincrono-projeto_inicial/projeto_inicial/admin/service/cliente-service.js
@@ -1,44 +1,27 @@
-const criaNovaLinha = (nome, email) => {
-  const linhaNovoCliente = document.createElement("tr");
-  const conteudo = `
-        <td class="td" data-td>${nome}</td>
-                    <td>${email}</td>
-                    <td>
-                        <ul class="tabela__botoes-controle">
-                            <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
-                            <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
-                        </ul>
-                    </td>
-                    `;
-
-  linhaNovoCliente.innerHTML = conteudo;
-  console.log(linhaNovoCliente);
-  return linhaNovoCliente;
-};
-
-const tabela = document.querySelector("[data-tabela]");
 
 const listaClientes = () => {
-  const promise = new Promise((resolve, reject) => {
-    const http = new XMLHttpRequest();
-    http.open("GET", "http://localhost:3000/profile");
+  return fetch(`http://localhost:3000/profile`).then((resposta) => {
+    return resposta.json();
+  })
+}
 
-    http.onload = () => {
-      if (http.status >= 400) {
-        reject(JSON.parse(http.response));
-      } else {
-        resolve(JSON.parse(http.response));
-      }
-    };
-    http.send();
-  });
-  return promise;
-};
-
-listaClientes()
-.then( data => {
-
-        data.forEach(elemento => {
-            tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email)); 
-        }) 
+const criaCliente = (nome, email) =>{
+  return fetch(`http://localhost:3000/profile`, {
+    method: 'POST',
+    headers: {
+      'Content-Type' : 'application/json'
+    },
+    body: JSON.stringify({
+      nome: nome,
+      email: email
     })
+  })
+  .then( resposta => {
+    return resposta.body
+  })
+}
+
+export const clienteService = {
+  listaClientes,
+  criaCliente
+}
